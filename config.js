f0a250167f27c9063da02f7bde9e6e18b9717296
jittervpn <diegogz143@gmail.com>
2026-06-26 13:04:00 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-// Este archivo se genera automáticamente.
-// En GitHub Pages: poner el token acá.
-// En Railway: el servidor usa variables de entorno y este archivo es ignorado.
-window.TMDB_TOKEN = window.TMDB_TOKEN || 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjI4YjJkNjVlZjVhNTBiMDhkOTkyZTBhN2MyZGU1NiIsIm5iZiI6MTc3NDk3MTEyOS43NTksInN1YiI6IjY5Y2JlOGY5YTljN2Q1NDIwN2FiYmNmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QwSGmNvfC-p9y1OGUsOfyKfMl29GkrFh_M4dd9Ct8ZQ';
-window.USE_BACKEND = false; // true = Railway backend | false = llamada directa a TMDB
+window.__TMDB_TOKEN__ = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjI4YjJkNjVlZjVhNTBiMDhkOTkyZTBhN2MyZGU1NiIsIm5iZiI6MTc3NDk3MTEyOS43NTksInN1YiI6IjY5Y2JlOGY5YTljN2Q1NDIwN2FiYmNmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QwSGmNvfC-p9y1OGUsOfyKfMl29GkrFh_M4dd9Ct8ZQ";
