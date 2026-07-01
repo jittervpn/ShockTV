5c80d0a1c6b0d4289bd2f52c0baec9f6298eac43
jittervpn <diegogz143@gmail.com>
2026-07-01 01:02:23 -0300

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,4 @@
+// SOLO para desarrollo local o GitHub Pages
+// Renombra este archivo a config.js y pon tu token
+// NUNCA subas config.js a GitHub (está en .gitignore)
+window.TMDB_TOKEN = 'PON_TU_TOKEN_AQUI';
