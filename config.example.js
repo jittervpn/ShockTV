41bccc20ac527bdbe771fdcb5a787d7312e48321
jittervpn <diegogz143@gmail.com>
2026-07-01 00:57:42 -0300

    Delete config.example.js

--- config.example.js
+++
@@ -1,4 +0,0 @@
-// SOLO para desarrollo local o GitHub Pages
-// Renombra este archivo a config.js y pon tu token
-// NUNCA subas config.js a GitHub (está en .gitignore)
-window.TMDB_TOKEN = 'PON_TU_TOKEN_AQUI';
